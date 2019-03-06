f42ebe11e3cbea6d927dc83ef13cc5210967b998
ctghadiri <ctghadiri@gmail.com>
2019-03-05 19:43:08 -0800

    pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// create form that takes in input with button
+// create on click function that takes in the val of input
+    // adds to array for buttons
+    // turns the array into buttons
+// create ajax link to giphy
+    // get url 
+    // rating info
+    // limit
+    // q
+// new buttons create 10 gifs of designated input
+    // append rating under gifs
+    // images should animate when clicked and stop as well
+// have a few buttons already made
